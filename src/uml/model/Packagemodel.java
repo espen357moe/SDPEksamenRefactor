6e9a0b711fb2d141a710dbcb68ba64e1d559531f
espen357moe <espen357moe@gmail.com>
2016-05-10 11:37:50 +0200

    Packagemodel oppdatert for å sørge for korrekt oppdatering av connections

--- src/uml/model/Packagemodel.java
+++ src/uml/model/Packagemodel.java
@@ -12,19 +12,29 @@ public class Packagemodel extends Figuremodel implements Cloneable {
 	public Packagemodel(int x, int y){
 		figureName = "Package";
 		start = new Point(x, y);
-		virtual = new Dimension(150, 80);
+		virtual = new Dimension(150, 100);
+		
+		Point p1 = new Point(10, 10);
+		Point p2 = new Point(400, 10);
+		Point p3 = new Point(400, 200);
+		Point p4 = new Point(10, 200);
+		
+		Point p5 = new Point(10, 200);
+		Point p6 = new Point(990, 200);
+		Point p7 = new Point(990, 990);
+		Point p8 = new Point(10, 990);
 		
 		// draw package tab
-		elements.add(new LineElementForPackagemodel(new Point(0,0), new Point(400,0), Hold.NONE));
-		elements.add(new LineElementForPackagemodel(new Point(400,0), new Point(400,200), Hold.NONE));
-		elements.add(new LineElementForPackagemodel(new Point(400, 200), new Point(0, 200), Hold.NONE));
-		elements.add(new LineElementForPackagemodel(new Point(0,200), new Point(0,0), Hold.NONE));
+		elements.add(new LineElementForPackagemodel(p1, p2, Hold.NONE));
+		elements.add(new LineElementForPackagemodel(p2, p3, Hold.NONE));
+		elements.add(new LineElementForPackagemodel(p3, p4, Hold.NONE));
+		elements.add(new LineElementForPackagemodel(p4, p1, Hold.NONE));
 		
 		// draw package body
-		elements.add(new LineElementForPackagemodel(new Point(0,200), new Point(1000,200), Hold.TRACE));
-		elements.add(new LineElementForPackagemodel(new Point(1000,200), new Point(1000,1000), Hold.TRACE));
-		elements.add(new LineElementForPackagemodel(new Point(1000,1000), new Point(0,1000), Hold.TRACE));
-		elements.add(new LineElementForPackagemodel(new Point(0,1000), new Point(0,200), Hold.TRACE));
+		elements.add(new LineElementForPackagemodel(p5, p6, Hold.TRACE));
+		elements.add(new LineElementForPackagemodel(p6, p7, Hold.TRACE));
+		elements.add(new LineElementForPackagemodel(p7, p8, Hold.TRACE));
+		elements.add(new LineElementForPackagemodel(p8, p5, Hold.TRACE));
 		
 		elements.add(new TextElement("Package", LabelAlign.CENTER));
 		
