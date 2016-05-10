5f31dec5ea87c246e815d88537c52978c2b41a27
Lorrides <tarhh@outlook.com>
2016-05-10 12:38:36 +0200

    Endter i Nodemodel sine ankerpunkt

--- src/figure/graphics/LineElementForNodeModel.java
+++ src/figure/graphics/LineElementForNodeModel.java
@@ -3,8 +3,6 @@ package figure.graphics;
 import java.awt.Point;
 import java.util.ArrayList;
 
-import figure.model.IFigure.Hold;
-
 public class LineElementForNodeModel extends LineElement {
 
 	public LineElementForNodeModel(Point a, Point b, Hold c) {
@@ -14,17 +12,17 @@ public class LineElementForNodeModel extends LineElement {
 	// lag på nytt når nødvendig
 	protected ArrayList<Point> traceresult() {
 		ArrayList<Point> pl = new ArrayList<Point>();
-		
+				
 		if(virtualstop.x - virtualstart.x != 0 && virtualstop.y - virtualstart.y == 0) { // x diff is !0 and y diff is 0 => line is horizontal
-			pl.add(new Point(realstart.x , realstart.y));
-			pl.add(new Point(realstop.x, realstop.y));	
-			pl.add(new Point(((realstart.x + realstop.x) / 2), (realstart.y)));
+			for (int i = 1; i < 4; i++){
+				pl.add(new Point (((realstop.x + realstart.x) / 4) * i, realstart.y));
+			}
 		}
 		else { // line is !horizontal
-			pl.add(new Point(realstart.x , realstart.y));
-			pl.add(new Point(realstop.x, realstop.y));
-			pl.add(new Point(((realstart.x)), (realstart.y + realstop.y) / 2));
-		}					
+			for(int i = 1; i < 4; i++) {
+				pl.add(new Point((realstart.x), ((realstop.y + realstart.y) / 4) * i));
+			}
+		}
 		return pl;
 	}
 
