1141ce311e1828dc5b1707709be665673df5563b
lillemy <elin.sorlie89@gmail.com>
2016-05-12 13:22:16 +0200

    Implementering av inerpolering

--- src/figure/graphics/LineElementForPackagemodel.java
+++ src/figure/graphics/LineElementForPackagemodel.java
@@ -9,19 +9,13 @@ public class LineElementForPackagemodel extends LineElement {
 		super(a, b, c);
 	}
 	
-	// lag på nytt når nødvendig
 	protected ArrayList<Point> traceresult() {
 		ArrayList<Point> pl = new ArrayList<Point>();
 			
 		pl.add(realstart);
 		pl.add(realstop);
-			
-		if(virtualstop.x - virtualstart.x != 0 && virtualstop.y - virtualstart.y == 0) { // x diff is !0 and y diff is 0 => line is horizontal		
-			pl.add(new Point(((realstop.x + realstart.x) / 2), realstart.y));														
-		}
-		else { // line is !horizontal
-			pl.add(new Point((realstart.x), (realstop.y + realstart.y) / 2));							
-		}					
+		pl.add(interpolate(realstart, realstop, 0.5));
+					
 		return pl;
 	}		
 }
